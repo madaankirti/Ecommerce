f716f814512b5175d5499673720d9086023a8e09
madaankirti <118740513+madaankirti@users.noreply.github.com>
2024-11-30 19:08:16 +0530

    Create CartContext.jsx

---
+++ src/contexts/CartContext.jsx
@@ -0,0 +1,27 @@
+// src/contexts/CartContext.js
+import React, { createContext, useContext, useState } from 'react';
+
+const CartContext = createContext();
+export const useCart = () => useContext(CartContext);
+
+export function CartProvider({ children }) {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = product => {
+    setCartItems([...cartItems, product]);
+  };
+
+  const removeFromCart = id => {
+    setCartItems(cartItems.filter(item => item.id !== id));
+  };
+
+  const clearCart = () => {
+    setCartItems([]);
+  };
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, clearCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
