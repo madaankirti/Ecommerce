ba8852d02c2adca0b0efc71a3a60b061259e5b14
madaankirti <118740513+madaankirti@users.noreply.github.com>
2024-11-30 19:08:52 +0530

    Create product.jsx

---
+++ src/data/product.jsx
@@ -0,0 +1,138 @@
+// src/data/products.js
+const products = [
+    {
+      id: 1,
+      name: 'Product One',
+      price: 29.99,
+      image: "./images/1.jpeg",
+    },
+    {
+      id: 2,
+      name: 'Product Two',
+      price: 49.99,
+      image: "/images/2.jpeg",
+    },
+    {
+      id: 3,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/3.jpeg",
+    },
+    {
+      id: 4,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/4.jpeg",
+    },
+    {
+      id: 5,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/5.jpeg",
+    },
+    {
+      id: 6,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/6.jpeg",
+    },
+    {
+      id: 7,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/7.jpeg",
+    },
+    {
+      id: 8,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/8.jpeg",
+    },
+    {
+      id: 9,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/9.jpeg",
+    },
+    {
+      id: 10,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/10.jpeg",
+    },
+    {
+      id: 11,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/11.jpeg",
+    },
+    {
+      id: 12,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/12.jpeg",
+    },
+    {
+      id: 13,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/13.jpeg",
+    },
+    {
+      id: 14,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/14.jpeg",
+    },
+    {
+      id: 15,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/15.jpeg",
+    },
+    {
+      id: 16,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/16.jpeg",
+    },
+    {
+      id: 17,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/17.jpeg",
+    },
+    {
+      id: 18,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/18.jpeg",
+    },
+    {
+      id: 19,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/19.jpeg",
+    },
+    {
+      id: 20,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/20.jpeg",
+    },
+    {
+      id: 21,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/21.jpeg",
+    },
+    {
+      id: 22,
+      name: 'Product Three',
+      price: 19.99,
+      image: "./images/22.jpeg",
+    },
+  ];
+  
+  export default products;
+  
